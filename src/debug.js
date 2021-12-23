7ddb16b5b38c489a3f25ff60314e4f31aabe185f
940842546 <940842546@users.noreply.github.com>
2021-12-23 23:36:10 +0800

    Update debug.js

--- src/debug.js
+++ src/debug.js
@@ -39,7 +39,7 @@ export function enableDebug(){
 
 export function updateDebugData(){
     if (global.settings.expose){
-        window.evolve.global = deepClone(global);
+        window.evolve.global = global;
         window.evolve.craftCost = deepClone(craftCost()),
         window.evolve.breakdown = deepClone(breakdown);
     }
